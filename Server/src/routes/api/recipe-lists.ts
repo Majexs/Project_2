6c3dfa2bd36eadf1deff2ca54680e7c5749fec40
Majexs <166327239+Majexs@users.noreply.github.com>
2024-10-30 20:40:52 -0400

    built out routes

--- Server/src/routes/api/recipe-lists.ts
+++ Server/src/routes/api/recipe-lists.ts
@@ -24,3 +24,90 @@ router.get('/', async (_req: Request, res: Response) => {
         });
     }
 });
+
+// GET /lists/:id - Get Lists by ID
+router.get('/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const list = await List.findByPk(id, {
+            include: [
+                {
+                    model: Recipe,
+                    as: 'includedRecipe',
+                    attributes: ['recipeName'],
+                },
+            ],
+        });
+        if (list) {
+            res.json(list);
+        } else {
+            res.status(404).json({
+                message: 'Recipe list not found'
+            });
+        }
+    } catch (error: any) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+});
+
+// POST /lists - Create new List
+router.post('/', async (req: Request, res: Response) => {
+    const { name, description } = req.body;
+    try {
+      const newList = await List.create({
+        name, description
+      });
+      res.status(201).json(newList);
+    } catch (error: any) {
+      res.status(400).json({
+        message: error.message
+      });
+    }
+});
+
+// PUT /lists/:id - Update List by ID
+router.put('/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { name, description } = req.body;
+    try {
+      const list = await List.findByPk(id);
+      if(list) {
+        list.name = name;
+        list.description = description;
+        await list.save();
+        res.json(list);
+      } else {
+        res.status(404).json({
+          message: 'List not found'
+        });
+      }
+    } catch (error: any) {
+      res.status(400).json({
+        message: error.message
+      });
+    }
+});
+
+// DELETE /lists/:id - Delete List by ID
+router.delete('/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      const list = await List.findByPk(id);
+      if(list) {
+        await list.destroy();
+        res.json({ message: 'List deleted' });
+      } else {
+        res.status(404).json({
+          message: 'List not found'
+        });
+      }
+    } catch (error: any) {
+      res.status(500).json({
+        message: error.message
+      });
+    }
+});
+  
+  export { router as favoriteRecipes };
