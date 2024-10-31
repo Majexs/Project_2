c144d3c2b33118636a5aa392354ff9bf255780bd
Majexs <166327239+Majexs@users.noreply.github.com>
2024-10-31 19:38:37 -0400

    built out models files

--- Server/src/models/index.ts
+++ Server/src/models/index.ts
@@ -1,4 +1,15 @@
 import sequelize from "../config/connection";
 import { RecipeFactory } from './recipe';
 import { RecipeListFactory } from './recipe-list';
-import { UserFactory } 
+import { UserFactory } from './user';
+
+    const Recipe = RecipeFactory(sequelize);
+    const List = RecipeListFactory(sequelize);
+    const User = UserFactory(sequelize);
+
+    User.hasMany(List, { foreignKey: 'assignedUserId'});
+    List.belongsTo(User, { foreignKey: 'assignedUserId', as: 'assignedUser'});
+    List.hasMany(Recipe, { foreignKey: 'assignedRecipeId'});
+    Recipe.belongsTo(List, { foreignKey: 'assignedRecipeId', as: 'assignedRecipe'});
+    
+export { User, List, Recipe };
