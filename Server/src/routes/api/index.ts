12a6fb13d501d96ccee2f13f1d4cdce6532ad301
Majexs <166327239+Majexs@users.noreply.github.com>
2024-10-30 21:10:42 -0400

    filled in details for Routes

--- Server/src/routes/api/index.ts
+++ Server/src/routes/api/index.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import { recipeRouter } from './recipe-search.js';
 import { favoriteRecipes } from './recipe-lists.js';
-import { user } from './users.js';
+import { userRouter } from './users.js';
 
 const router = Router();
 
 router.use('/recipes', recipeRouter);
 router.use('/favorites', favoriteRecipes);
-router.use('/users', user);
+router.use('/users', userRouter);
 
 export default router;
